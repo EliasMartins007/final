22fc3626b1082283bd02766c6505e2dbe0b4636b
EliasMartins007 <eliasfaculdade@yahoo.com.br>
2021-09-03 20:47:25 -0300

    login

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,5 +3,24 @@ class RegistrationsController < ApplicationController
       #teste user
     @user = User.new
   end
+
+  def create
+    #render plain: params[:user]
+    @user = User.new(user_params)
+     if @user.save
+       session[:user_id] = @user.id
+       redirect_to root_path, notice: "LOGIN REALIZADO COM SUCESSO !"
+     else
+      #flash[:alert] = "algo esta errado"
+      render :new
+     end  
+
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation) 
+  end  
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
   #get "aliens", to: "aliens#index"
   get "sign_up", to: "registrations#new"
-  post "sign_up", to: "registrtions#create"
+  post "sign_up", to: "registrations#create"
   #
   root to: "main#index"
  
