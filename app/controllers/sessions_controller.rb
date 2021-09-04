0f0945c630365868e35cc0bd78541d1728e09d4f
EliasMartins007 <eliasfaculdade@yahoo.com.br>
2021-09-03 22:57:26 -0300

    login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+    #
+  end  
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+       session[:user_id] = user.id
+       redirect_to root_path, notice: "Login com sucesso !"
+    else
+        flash[:alert] = "email ou senha invalidos !"
+        render :new
+    end       
+  end  
+  
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: "Desconectado !"
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   post "sign_up", to: "registrations#create"
   delete "logout", to: "sessions#destroy"
   #
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  
+  #
   root to: "main#index"
  
 end
