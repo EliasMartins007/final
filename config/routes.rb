492c855790216dafdbc76f748001d0e1bc786924
EliasMartins007 <eliasfaculdade@yahoo.com.br>
2021-09-03 21:08:17 -0300

    logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   #get "aliens", to: "aliens#index"
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
   #
   root to: "main#index"
  
